c256445983410ec27c860728f2e5e7b16e1de70b
Masooda Shareefy <155033090+masoodashareefy@users.noreply.github.com>
2024-12-24 11:33:16 -0700

    Upload Oracle database stored procedures and views

---
+++ Urban-Greening-Initiative-Oracle-Database.sql
@@ -0,0 +1,552 @@
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('IL04 5405 6645 3279 6862 127', 'Micaela Kingsly', '105-688-9961',
+'mkingsly4e@symantec.com', 'Subcontracting', 'AL06 6240 1308 XOVP EXXJ
+RHK7 BWUE');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('LV65 THVH CTOB 4Q9Q HZTI M', 'Stevy Bussen', '260-468-5357',
+'sbussen4f@shinystat.com', 'CGL', 'FR28 4062 4747 78CT UYBO A4PB 842');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('NL02 EGER 9801 5122 62', 'Karlik Ardern', '375-980-5754',
+'kardern4g@yolasite.com', 'PeopleSoft', 'FR36 0733 0610 567M PFIG AI9C
+T53');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('FR16 6001 4369 55DX T7T3 Z4XE Q94', 'Trevor Brobyn', '812-725-7806',
+'tbrobyn4h@hubpages.com', 'WMI', 'LB14 4509 1VER 0DIZ UXUY E5MG KSW8');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('AE29 9724 0877 7203 0430 426', 'Tanner Devo', '216-199-0809',
+'tdevo4i@yahoo.com', 'RPG III', 'MU25 XPAA 8423 8947 3290 1971 644F SL');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('TR66 7448 1NFH TL66 VC5U HDIK T8', 'Cassondra Shewry', '230-770-
+3983', 'cshewry4j@slideshare.net', 'VPM', 'PS34 SHXG L2CI OKBR SMEG 6XBK
+WEZD I');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('FR88 5347 6203 52SW S0BF FBB8 Q29', 'Gifford Drohan', '117-665-
+6831', 'gdrohan4k@lulu.com', 'Microsoft Dynamics', 'BE05 5073 6765 0407');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('MD67 4X7X ZQCQ LZAJ TYFJ BIHG', 'Nikoletta Allcock', '981-700-8258',
+'nallcock4l@patch.com', 'Zombies', 'FR09 0629 3173 95TS B20I 8RV2 C91');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('LI07 7660 9DRB I7WK LTAE T', 'Sharona Spybey', '695-398-0769',
+'sspybey23@wunderground.com', 'Internal Controls', 'VG36 JERI 6287 4148
+3921 8322');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('RO85 RLXL 93IP B8DV F82L Y8AO', 'Camellia Hoogendorp', '461-336-
+9559', 'choogendorp24@cisco.com', 'DDoS Mitigation', 'GR57 7153 087F TFTP
+VTCL E09G YEU');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('IS27 6154 4376 5753 0814 8532 44', 'Wilek Loveman', '815-120-3296',
+'wloveman25@army.mil', 'Sh', 'AE71 3157 2618 8164 1633 717');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('BR83 7070 0327 1218 1899 0938 073G S', 'Olympia Reynoldson', '173-
+906-7318', 'oreynoldson26@uiuc.edu', 'Mobile Applications', 'LI96 7902
+2E3P VMNI 4NIV R');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('HU11 5322 3962 7025 6494 7874 8525', 'Lindsay Rieme', '360-369-
+8762', 'lrieme27@virginia.edu', 'Following Up', 'BA42 4808 6467 3506
+6938');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('RO21 MDOP BWJR HNSJ ZPGQ EZQG', 'Efren Brittles', '235-390-9102',
+'ebrittles28@netlog.com', 'Jeep', 'BE88 6838 0612 9004');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('MC86 7811 6170 055G GNNQ QCE9 R54', 'Yuma Risborough', '226-431-
+7509', 'yrisborough29@google.com.br', 'MSP Practitioner', 'GL84 2886 0511
+2443 50');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('IT03 F097 5607 982R SLKU JON1 RFW', 'Aggie Egdell', '897-276-4843',
+'aegdell2a@tumblr.com', 'Landlords', 'BG69 PEVJ 7457 54CZ U5WE VK');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('BG67 SKLL 2856 04RU GADL ZR', 'Krisha Angelo', '518-408-5751',
+'kangelo2b@rediff.com', 'Basic HTML', 'MK18 0667 BA66 EX7N P97');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('ME46 8627 8662 4680 9763 11', 'Coral Quincey', '754-298-4270',
+'cquincey2c@marriott.com', 'DNA fingerprinting', 'AZ94 IDWV KFC1 H6V4 XYDJ
+HUDL MTZK');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('FO38 8219 0048 0528 16', 'Mead Hambribe', '379-573-3724',
+'mhambribe2d@rambler.ru', 'iWay', 'RO12 PUEI FPQD 9WZ5 GW9K ZAOO');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('LI52 5010 37FJ L1HI ASMT 3', 'Floyd Moan', '522-102-1870',
+'fmoan2e@myspace.com', 'Omnibus', 'LB81 0441 QM25 AWCV EMRU S7JR NO8C');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('CZ03 0698 9101 7927 6444 4595', 'Rae Joul', '412-978-7814',
+'rjoul2f@blogs.com', 'Mobile Applications', 'GI70 CDFK GZBM QHWA ACNL
+X92');
+Insert Into volunteers
+(volunteerid, fullname, contactnumber, email, skills, projectid)
+Values
+('NO43 0173 9149 514', 'Dorian Figgures', '286-104-5081',
+'dfiggures2g@com.com', 'Airworthiness', 'KW16 MHMW JHND YPHC UEII 2NQL
+FIWJ BL');
+-- Insert records into Donors Table
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('DE74 9179 7309 0973 7902 21', 'Erdman-Witting', 'Individual',
+'rglasby0@hp.com', '6425.35', 'FR41 0421 3241 92O4 GVZP YRH8 W51');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('DO81 YVTQ 1240 3906 9207 2198 2616', 'Bednar, Runolfsson and
+Fritsch', 'Individual', 'ncorstan48@acquirethisname.com', '5610.48', 'NL20
+XOCS 2397 1597 23');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('MR09 0921 7996 7822 7731 3879 745', 'Christiansen Group',
+'Individual', 'rscahill49@boston.com', '4156.64', 'LB49 7340 2TJF APSG
+ORIL ZKBA OOTS');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('PK16 AYIB PC9K MELO 6RZY 0B2A', 'Welch Group', 'Organization',
+'murpeth4a@tripod.com', '9172.09', 'FR63 4473 6500 54FT W323 SSTW E93');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('AD24 7982 3049 ALWD S0UV BW6E', 'Veum, Wiegand and Thompson',
+'Company', 'agai4b@amazon.de', '9428.11', 'CZ50 7354 1847 5295 1115
+9775');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('KW84 GTQC E3UA TVV7 SH9C SZTS T5CZ NF', 'Herman-Spinka',
+'Organization', 'hreichardt4c@globo.com', '5828.09', 'CY23 8663 9650 J6S2
+NRWZ QL7E GWMJ');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('DK20 6323 7125 2596 97', 'Halvorson, Berge and Spencer',
+'Individual', 'kpeebles4d@virginia.edu', '9030.08', 'CH74 3081 10GK 5GZ9
+AZKG T');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('GT49 JKWQ WRVH IKB9 CTYA RER7 R0V1', 'Nader, Nader and Adams',
+'Individual', 'mkingsly4e@pbs.org', '1593.31', 'AL06 6240 1308 XOVP EXXJ
+RHK7 BWUE');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('FR74 4189 1819 80LG XTSA H9P4 R48', 'Kuhn LLC', 'Organization',
+'sbussen4f@state.gov', '1968.86', 'FR28 4062 4747 78CT UYBO A4PB 842');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('MU97 KMVM 8801 6912 0976 7564 528R BL', 'Becker, Lindgren and
+Graham', 'Individual', 'kardern4g@google.cn', '6272.29', 'FR36 0733 0610
+567M PFIG AI9C T53');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('FR61 4666 7178 00R6 BXQS FRZZ M56', 'Kulas LLC', 'Organization',
+'tbrobyn4h@com.com', '8052.93', 'LB14 4509 1VER 0DIZ UXUY E5MG KSW8');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('GI91 MHNU IDQZ BXSX R4Q4 S2N', 'Morissette-Hilpert', 'Individual',
+'tdevo4i@cornell.edu', '1628.47', 'MU25 XPAA 8423 8947 3290 1971 644F
+SL');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('FR37 7755 2512 31F7 40Z3 DB3C E74', 'Blick-Greenholt', 'Individual',
+'cshewry4j@reference.com', '9654.38', 'PS34 SHXG L2CI OKBR SMEG 6XBK WEZD
+I');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('BA58 2957 1550 8453 7530', 'Tremblay Group', 'Organization',
+'gdrohan4k@paginegialle.it', '1114.41', 'BE05 5073 6765 0407');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('FR52 2244 7061 44B3 5H5A PPHU P11', 'Murphy-Kuphal', 'Individual',
+'nallcock4l@dropbox.com', '7064.66', 'FR09 0629 3173 95TS B20I 8RV2 C91');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('IS69 6673 3277 9802 0910 9120 65', 'Ryan, Reichert and Osinski',
+'Individual', 'mfrankcom2x@last.fm', '1869.48', 'VG36 JERI 6287 4148 3921
+8322');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('FO48 5271 6172 2655 10', 'Conn and Sons', 'Company',
+'pbalwin2y@multiply.com', '2978.52', 'GR57 7153 087F TFTP VTCL E09G YEU');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('HU44 1429 5181 7962 4153 0630 7894', 'Nikolaus LLC', 'Company',
+'kdrane2z@ebay.com', '6374.09', 'AE71 3157 2618 8164 1633 717');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('HR92 2257 2218 6616 3100 8', 'Bernhard-Wolf', 'Company',
+'esillitoe30@hexun.com', '91.44', 'LI96 7902 2E3P VMNI 4NIV R');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('BG30 HDWF 8473 01CR DZ2W AS', 'Wuckert, Bartoletti and Cronin',
+'Individual', 'fbertome31@sun.com', '2327.97', 'BA42 4808 6467 3506
+6938');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('HU43 2754 2809 5414 1636 5450 3932', 'Rath-Braun', 'Individual',
+'rmichelle32@ameblo.jp', '9310.22', 'BE88 6838 0612 9004');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('EE34 2419 7340 5912 1550', 'Zieme and Sons', 'Company',
+'bvanckov33@gnu.org', '5980.54', 'GL84 2886 0511 2443 50');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('EE62 9024 2060 0637 8405', 'Kutch-Ryan', 'Organization',
+'lseres34@apache.org', '8242.37', 'BG69 PEVJ 7457 54CZ U5WE VK');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('CZ15 8591 7304 1603 7493 7562', 'Hamill, Bahringer and Dooley',
+'Organization', 'sheeran35@163.com', '540.21', 'MK18 0667 BA66 EX7N P97');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('IT40 J011 8036 048W R9XP ZE3V Q6U', 'Connelly, Bayer and Zemlak',
+'Individual', 'seddowis36@webmd.com', '3912.54', 'AZ94 IDWV KFC1 H6V4 XYDJ
+HUDL MTZK');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('ME28 5811 2530 1802 7327 13', 'Hartmann and Sons', 'Organization',
+'bbentsen37@elegantthemes.com', '4969.04', 'RO12 PUEI FPQD 9WZ5 GW9K
+ZAOO');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('AE26 9818 1577 4236 6524 599', 'Krajcik, Carroll and Casper',
+'Organization', 'lmcconigal38@mozilla.org', '457.47', 'LB81 0441 QM25 AWCV
+EMRU S7JR NO8C');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('CH51 3290 4RSB UQWT OWPR M', 'Rowe, Metz and Gleichner',
+'Individual', 'cjankovsky39@flavors.me', '6970.26', 'GI70 CDFK GZBM QHWA
+ACNL X92');
+Insert Into Donors
+(donorid, donorname, typeofdonor, donorcontact, amountdonated,
+projectid)
+Values
+('LI27 8799 8LTN ZS3T K6FB C', 'Morissette-Hudson', 'Organization',
+'ewinley3a@newyorker.com', '5497.61', 'KW16 MHMW JHND YPHC UEII 2NQL FIWJ
+BL');
+-- Project Part 3 - Stored Procedures
+-- Stored procedure 1
+CREATE OR REPLACE PROCEDURE GetSuppliesByProjectName
+(
+p_ProjectName IN Projects.ProjectName%TYPE
+)
+IS
+/*
+DESCRIPTION: Provides a list of items required for a specefic project and
+their costs.
+EXAMPLE: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE GetSuppliesByProjectName('ParkRestoration');
+*/
+BEGIN
+DBMS_OUTPUT.PUT_LINE('Project Name' || ', ' || 'Supply Item' || ', '
+|| 'Cost');
+FOR rec IN (SELECT p.ProjectID, p.ProjectName, s.SupplyID, s.Item,
+s.CostPerItem
+FROM Projects p
+JOIN Supplies s ON p.ProjectID = s.ProjectID
+WHERE p.ProjectName = p_ProjectName) LOOP
+DBMS_OUTPUT.PUT_LINE(rec.ProjectName || ', ' || rec.Item || ', '
+|| rec.CostPerItem);
+END LOOP;
+END GetSuppliesByProjectName;
+/
+-- Stored Procedure 2
+CREATE OR REPLACE PROCEDURE ListVolunteersAndCountByProject
+IS
+/*
+DESCRIPTION: Lists all volunteers by project and includes a count of
+volunteers for each project.
+Example: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE ListVolunteersAndCountByProject();
+*/
+BEGIN
+FOR rec IN (SELECT p.ProjectName,
+v.FullName,
+(SELECT COUNT(*)
+FROM Volunteers v2
+WHERE v2.ProjectID = p.ProjectID) AS
+VolunteerCount
+FROM Projects p
+JOIN Volunteers v ON p.ProjectID = v.ProjectID
+ORDER BY p.ProjectName, v.FullName) LOOP
+DBMS_OUTPUT.PUT_LINE('Project: ' || rec.ProjectName ||
+' - Volunteer: ' || rec.FullName ||
+' - Total Volunteers in Project: ' ||
+rec.VolunteerCount);
+END LOOP;
+END ListVolunteersAndCountByProject;
+/
+-- Stored Procedure 3
+CREATE OR REPLACE PROCEDURE ProjectDonationsAndSupplies IS
+v_project_id Projects.ProjectID%TYPE;
+v_data VARCHAR2(100);
+/*
+DESCRIPTION: Provides a financial summary for each project by listing the
+total amount donated and the total cost of supplies.
+Example: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE ProjectDonationsAndSupplies;
+*/
+BEGIN
+DBMS_OUTPUT.PUT_LINE(RPAD('Project ID', 15) || RPAD('Type', 20) ||
+'Amount');
+FOR donation_rec IN (
+SELECT ProjectID, 'Donations' AS DataType, SUM(AmountDonated) AS
+TotalAmount
+FROM Donors
+GROUP BY ProjectID
+UNION
+SELECT ProjectID, 'Supplies Cost' AS DataType, SUM(CostPerItem *
+QuantityAvailable) AS TotalAmount
+FROM Supplies
+GROUP BY ProjectID
+) LOOP
+v_project_id := donation_rec.ProjectID;
+v_data := RPAD(donation_rec.DataType, 20) || donation_rec.TotalAmount;
+DBMS_OUTPUT.PUT_LINE(RPAD(v_project_id, 15) || v_data);
+END LOOP;
+END ProjectDonationsAndSupplies;
+/
+-- Store Procedure 4 - Transactional
+CREATE OR REPLACE PROCEDURE AddDonorToProject
+(
+p_donor_id IN Donors.DonorID%TYPE,
+p_project_id IN Projects.ProjectID%TYPE,
+p_donor_name IN Donors.DonorName%TYPE,
+p_type_of_donor IN Donors.TypeOfDonor%TYPE,
+p_donor_contact IN Donors.DonorContact%TYPE,
+p_amount_donated IN Donors.AmountDonated%TYPE
+)
+IS
+/*
+DESCRIPTION: Adds a new donor to the Donors table each time a donation is
+made to a specific project. It helps to keep track of all new donations.
+Example: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE AddDonorToProject('AM00 1111 1111 1000 0000 0', 'LI96 7902
+2E3P VMNI 4NIV R', 'Amelia Griffith', 'Individual',
+'agriffith@example.com', 500);
+*/
+BEGIN
+INSERT INTO Donors (DonorID, ProjectID, DonorName, TypeOfDonor,
+DonorContact, AmountDonated)
+VALUES (p_donor_id, p_project_id, p_donor_name, p_type_of_donor,
+p_donor_contact, p_amount_donated);
+DBMS_OUTPUT.PUT_LINE('Donor ' || p_donor_name || ' added successfully
+to project ' || p_project_id || ' with a donation of ' ||
+p_amount_donated);
+COMMIT;
+EXCEPTION
+WHEN OTHERS THEN
+ROLLBACK;
+DBMS_OUTPUT.PUT_LINE('Transaction failed. Error: ' || SQLERRM);
+END AddDonorToProject;
+/
+-- Stored procedure 5 Using a CASE Statement
+CREATE OR REPLACE PROCEDURE ListProjectStatusCategories
+IS
+/*
+DESCRIPTION: lists all projects and categorizes their status into
+'Active', 'Pending', or 'Closed'
+Example: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE ListProjectStatusCategories;
+*/
+BEGIN
+DBMS_OUTPUT.PUT_LINE(RPAD('Project ID', 15) || RPAD('Project Name',
+20) || 'Status Category');
+FOR rec IN (
+SELECT ProjectID, ProjectName, Status,
+CASE
+WHEN Status = 'In Progress' THEN 'Active'
+WHEN Status = 'Pending' THEN 'Pending'
+ELSE 'Closed'
+END AS StatusCategory
+FROM Projects
+) LOOP
+DBMS_OUTPUT.PUT_LINE(RPAD(rec.ProjectID, 15) ||
+RPAD(rec.ProjectName, 20) || rec.StatusCategory);
+END LOOP;
+END ListProjectStatusCategories;
+/
+-- Stored Procedure 6 Using Aggregate & Having
+CREATE OR REPLACE PROCEDURE AverageCostPerProject
+IS
+/*
+DESCRIPTION: Calculates the average cost per item for supplies used in
+each project
+and filters to include only those projects where the average cost
+exceeds $500.
+Example: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE AverageCostPerProject;
+*/
+v_project_id Projects.ProjectID%TYPE;
+v_average_cost NUMBER;
+BEGIN
+DBMS_OUTPUT.PUT_LINE(RPAD('Project ID', 15) || 'Average Cost Per
+Item');
+FOR rec IN (
+SELECT ProjectID, AVG(CostPerItem) AS AverageCostPerItem
+FROM Supplies
+GROUP BY ProjectID
+HAVING AVG(CostPerItem) > 500 -- Only select projects with an
+average cost > $500
+) LOOP
+v_project_id := rec.ProjectID;
+v_average_cost := rec.AverageCostPerItem;
+DBMS_OUTPUT.PUT_LINE(RPAD(v_project_id, 15) || v_average_cost);
+END LOOP;
+END AverageCostPerProject;
+/
+-- Stored Procedure 7 Transaction Query
+CREATE OR REPLACE PROCEDURE CorrectProjectName
+IS
+/*
+DESCRIPTION: Corrects the misspelling of the word "Garden" which I spelled
+wrong in my data as "Graden" to "Garden"
+Example: To run the query, please run the two lines below:
+SET SERVEROUTPUT ON;
+EXECUTE CorrectProjectName;
+*/
+BEGIN
+SAVEPOINT start_correction;
+UPDATE Projects
+SET ProjectName = 'Garden'
+WHERE ProjectName = 'Graden';
+DBMS_OUTPUT.PUT_LINE('Rows updated: ' || SQL%ROWCOUNT);
+COMMIT;
+DBMS_OUTPUT.PUT_LINE('Update committed successfully.');
+EXCEPTION
+WHEN OTHERS THEN
+ROLLBACK TO start_correction;
+DBMS_OUTPUT.PUT_LINE('Error encountered: ' || SQLERRM);
+RAISE;
+END CorrectProjectName;
+/
+-- Project Views
+-- Project View 1 - Project Overview
+CREATE OR REPLACE VIEW SimpleProjectDetails AS
+SELECT
+/*
+To run the query, please run the link below:
+-- SELECT * FROM SimpleProjectDetails;
+*/
+p.ProjectID,
+p.ProjectName,
+p.Status,
+l.Address,
+l.ZipCode,
+pl.CommonName AS Plant,
+pl.ScientificName AS PlantScientificName
+FROM
+Projects p
+JOIN
+Locations l ON p.LocationID = l.LocationID
+JOIN
+Plants pl ON p.PlantID = pl.PlantID;
+-- View 2: ProjectFinancials
+CREATE OR REPLACE VIEW ProjectFinancialOverview AS
+SELECT
+/*
+To run the query, please run the link below:
+-- SELECT * FROM ProjectFinancialOverview;
+*/
+p.ProjectID,
+p.ProjectName,
+SUM(d.AmountDonated) AS TotalDonations,
+COUNT(d.DonorID) AS NumberOfDonations
+FROM
+Projects p
+LEFT JOIN
+Donors d ON p.ProjectID = d.ProjectID
+GROUP BY
+p.ProjectID, p.ProjectName;
